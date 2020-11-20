0ec79be30a1b34238cf212ed171d4e2e8b4a12c1
kweaver87 <28742312+KWeaver87@users.noreply.github.com>
2020-11-20 00:17:04 -0500

    Move files to src folder

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,12 @@
+import React from 'react'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
+}
+MyApp.propTypes = {
+  Component: React.Component,
+  pageProps: {},
 }
 
 export default MyApp
