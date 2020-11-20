3a6ac02a3d9a9b37e841235d85b93d3073081925
kweaver87 <28742312+KWeaver87@users.noreply.github.com>
2020-11-20 00:45:43 -0500

    Fixes links

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,13 @@ export default function Home(): JSX.Element {
     <div>
       <main>
         <h1>
-          Welcome to <a href="KWEAVE.NET">KWEAVE.NET!</a>
+          Welcome to <a href="https://kweave.net/site/">KWEAVE.NET</a>!
         </h1>
 
         <p>This is all just placeholder until I create some actual content!</p>
         <p>
-          But you can see my resume in <a href="/resume.html">HTML</a> or{' '}
-          <a href="/resume.pdf">PDF</a> formats.
+          But you can see my resume in <a href="resume.html">HTML</a> or{' '}
+          <a href="resume.pdf">PDF</a> formats.
         </p>
       </main>
     </div>
