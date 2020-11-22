7708c7484e6ea820bcdbade1f5e61a468515c553
kweaver87 <28742312+KWeaver87@users.noreply.github.com>
2020-11-22 11:41:58 -0500

    Rewording

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ export default function Home(): JSX.Element {
 
         <p>This is all just placeholder until I create some actual content!</p>
         <p>
-          But you can see my resume in <a href="resume.html">HTML</a> or{' '}
+          For now, you can see my resume in <a href="resume.html">HTML</a> or{' '}
           <a href="resume.pdf">PDF</a> formats.
         </p>
       </main>
