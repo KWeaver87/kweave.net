58035fd6decc453555dc16e74e8c59e86a6921cb
kweaver87 <28742312+KWeaver87@users.noreply.github.com>
2021-06-10 13:16:44 -0400

    Set deploy script to abort on error

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # Publishes site via SSH
 
+# Immediately exits script if any command gives non-zero status
+set -e
+
 tar -czf out.tar.gz -C out .
 scp -r out.tar.gz kweave.net:~/www.tar.gz
 ssh kweave.net 'tar -zxf www.tar.gz -C www'
