933ad0c55a17c5636fdf3224f9fe7b89572851d8
KWeaver87 <28742312+KWeaver87@users.noreply.github.com>
2021-12-12 19:36:26 -0500

    Replaces eslint-plugins with eslint-plugin-next

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,13 +10,7 @@ module.exports = {
   }, // to enable features such as async/await
   settings: { react: { version: 'detect' } },
   ignorePatterns: ['node_modules/*', '.next/*', '.out/*', '!.prettierrc.js'], // We don't want to lint generated files nor node_modules, but we want to lint .prettierrc.js (ignored by default by eslint)
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended', // React rules
-    'plugin:react-hooks/recommended', // React hooks rules
-    'plugin:jsx-a11y/recommended', // Accessibility rules
-    'plugin:prettier/recommended',
-  ],
+  extends: ['eslint:recommended', 'next/core-web-vitals', 'plugin:prettier/recommended'],
   overrides: [
     // This configuration will apply only to TypeScript files
     {
@@ -29,16 +23,11 @@ module.exports = {
       },
       extends: [
         'eslint:recommended',
-        'plugin:@typescript-eslint/recommended', // TypeScript rules
-        'plugin:react/recommended', // React rules
-        'plugin:react-hooks/recommended', // React hooks rules
-        'plugin:jsx-a11y/recommended', // Accessibility rules
+        'plugin:@typescript-eslint/recommended',
+        'next/core-web-vitals',
         'plugin:prettier/recommended',
       ],
       rules: {
-        'react/prop-types': 'off', // We will use TypeScript's types for component props instead
-        'react/react-in-jsx-scope': 'off', // No need to import React when using Next.js
-        'jsx-a11y/anchor-is-valid': 'off', // This rule is not compatible with Next.js's <Link /> components
         '@typescript-eslint/no-unused-vars': ['error'], // Why would you want unused vars?
         '@typescript-eslint/explicit-function-return-type': [
           // I suggest this setting for requiring return types on functions only where usefull
